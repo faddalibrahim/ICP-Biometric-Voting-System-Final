5cf6e4dc80fc22273e07f206345dc89d15ff4c14
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-03-08 22:20:28 +0000

    connect login page and main page

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,19 +1,23 @@
 package controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController extends Application{
+public class LoginController extends Application {
 
     @FXML
     private ResourceBundle resources;
@@ -22,17 +26,25 @@ public class LoginController extends Application{
     private URL location;
 
     @FXML
-    private JFXTextField voterID;
+    private Label loginLoginLabel;
 
     @FXML
-    private JFXButton authenticateButton;
+    private JFXTextField loginVoterID;
+
+    @FXML
+    private JFXButton loginAuthenticateButton;
+
+    @FXML
+    private Label loginAuthenticationNotification;
+
 
     @FXML
     void initialize() {
-
-        authenticateButton.setOnAction(actionEvent -> {
-
+        loginAuthenticateButton.setOnAction(actionEvent -> {
+            authenticateVoter();
         });
+
+        loginAuthenticationNotification.setVisible(false);
     }
 
     @Override
@@ -47,4 +59,27 @@ public class LoginController extends Application{
     public static void main(String[] args) {
         launch(args);
     }
+
+    /* Helper Methods */
+
+    //This is just a basic authentication for when I was connecting the scenes. Change it to your own authentication (checking the database)
+    private void authenticateVoter() {
+        //getting the main window
+        loginAuthenticateButton.getScene().getWindow().hide();
+
+        if(!loginVoterID.getText().toString().trim().equals("")){
+            Stage mainPageStage = new Stage();
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/view/mainPage.fxml"));
+                Scene mainPageScene = new Scene(root, 700, 400);
+                mainPageStage.setScene(mainPageScene);
+                mainPageStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
 }
