0edf062c8bd27f465c75914b98502b74b151455a
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2021-03-09 04:44:03 +0000

    interfacing with database

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("./view/mainPage.fxml"));
+            Scene scene = new Scene(root);
+            //scene.getStylesheets().add("styles.css");
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
